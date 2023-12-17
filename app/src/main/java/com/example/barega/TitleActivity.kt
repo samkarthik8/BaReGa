698d0caf2f50655894ea774701eab8b015a0cb81
Karthik Sampath (TCS) <k.s10@tcs.com>
2023-12-18 04:53:56 +0530

    Added BaReGa code

--- app/src/main/java/com/example/barega/TitleActivity.kt
+++ app/src/main/java/com/example/barega/TitleActivity.kt
@@ -1,12 +1,15 @@
 package com.example.barega
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class TitleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,23 +19,32 @@ class TitleActivity : AppCompatActivity() {
         val ba = "Ba"
         val re = "Re"
         val ga = "Ga"
-        val coloredText = SpannableString("$ba $re $ga")
+        val coloredText = SpannableString("$ba$re$ga")
         coloredText.setSpan(
-            ForegroundColorSpan(Color.BLUE), 0, ba.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        coloredText.setSpan(
-            ForegroundColorSpan(Color.RED),
-            ba.length + 1,
-            ba.length + re.length + 1,
+            ForegroundColorSpan(ContextCompat.getColor(this, android.R.color.holo_blue_light)),
+            0,
+            ba.length,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
         coloredText.setSpan(
-            ForegroundColorSpan(Color.GREEN),
-            ba.length + re.length + 2,
-            ba.length + re.length + ga.length + 2,
+            ForegroundColorSpan(ContextCompat.getColor(this, android.R.color.holo_red_light)),
+            ba.length,
+            ba.length + re.length,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         )
-
+        coloredText.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(this, android.R.color.holo_green_light)),
+            ba.length + re.length,
+            ba.length + re.length + ga.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
         textView.text = coloredText
+
+    }
+
+    fun startGame(view: View) {
+        // Start GameActivity
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
     }
 }
