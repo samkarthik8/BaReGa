cee694dba2ba89774ededd3770a5da12e040d4f6
Karthik Sampath (TCS) <k.s10@tcs.com>
2023-12-18 09:38:55 +0530

    Added BaReGa code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.code.gson:gson:2.10")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
