791c78f2219016158bf8eed5f4aa7032f01339f1
Karthik Sampath (TCS) <k.s10@tcs.com>
2023-12-20 15:43:41 +0530

    Added BaReGa code

--- app/src/main/java/com/example/barega/SettingsActivity.kt
+++ app/src/main/java/com/example/barega/SettingsActivity.kt
@@ -1,11 +1,22 @@
+@file:Suppress("unused")
+
 package com.example.barega
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        setContentView(R.layout.activity_settings)
+    }
+    @Suppress("UNUSED_PARAMETER")
+    fun onBackButtonClick(view: View) {
+        // Start TitleActivity
+        val intent = Intent(this, TitleActivity::class.java)
+        startActivity(intent)
+        finish() // Finish the activity immediately after starting TitleActivity
     }
 }
