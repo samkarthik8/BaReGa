021b2b0375f322f895294c5db289c1deb458c23c
Karthik Sampath (TCS) <k.s10@tcs.com>
2023-12-21 19:12:07 +0530

    Added BaReGa code

--- app/src/main/java/com/example/barega/UtilsActivity.kt
+++ app/src/main/java/com/example/barega/UtilsActivity.kt
@@ -1,11 +1,13 @@
 package com.example.barega
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 object Utils {
+    @SuppressLint("DiscouragedApi")
     fun setTextViewBackgroundColor(
         context: Context,
         textViewId: String,
@@ -14,11 +16,8 @@ object Utils {
         blue: Int
     ) {
         val resourceId = context.resources.getIdentifier(textViewId, "id", context.packageName)
-
-        if (resourceId != 0) {
-            val textView = (context as AppCompatActivity).findViewById<TextView>(resourceId)
-            val backgroundColor = Color.rgb(red, green, blue)
-            textView.setBackgroundColor(backgroundColor)
-        }
+        val textView = (context as AppCompatActivity).findViewById<TextView>(resourceId)
+        val backgroundColor = Color.rgb(red, green, blue)
+        textView.setBackgroundColor(backgroundColor)
     }
 }
