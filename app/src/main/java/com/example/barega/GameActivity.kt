bafbc240890aa014666f6ca6f0bdcfb0735c5f92
Karthik Sampath (TCS) <k.s10@tcs.com>
2023-12-21 18:40:42 +0530

    Added BaReGa code

--- app/src/main/java/com/example/barega/GameActivity.kt
+++ app/src/main/java/com/example/barega/GameActivity.kt
@@ -1,11 +1,17 @@
 package com.example.barega
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        val questionColorView = findViewById<TextView>(R.id.questionColorSection)
+        // Set the question color
+        questionColorView.setBackgroundColor(Color.rgb(128, 0, 128))
     }
 }
