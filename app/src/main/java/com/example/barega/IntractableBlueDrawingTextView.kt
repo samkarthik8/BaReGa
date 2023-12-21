b7f757524361a83b9fe1a18592db14fdbb05fc87
Karthik Sampath (TCS) <k.s10@tcs.com>
2023-12-21 19:45:27 +0530

    Added BaReGa code

---
+++ app/src/main/java/com/example/barega/IntractableBlueDrawingTextView.kt
@@ -0,0 +1,57 @@
+package com.example.barega
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatTextView
+
+class IntractableBlueDrawingTextView : AppCompatTextView {
+    private var dotX: Float = 0f
+    private var dotY: Float = 0f
+    private val dotRadius = 10f
+    private val dotPaint = Paint()
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        dotPaint.color = Color.RED
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        // Draw the dot on the canvas
+        canvas?.drawCircle(dotX, dotY, dotRadius, dotPaint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                // Update the dot position when the user touches or moves
+                dotX = event.x
+                dotY = event.y
+                // Invalidate the view to trigger a redraw
+                invalidate()
+                return true
+            }
+
+            else -> return super.onTouchEvent(event)
+        }
+    }
+}
