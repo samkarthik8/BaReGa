91d84a69fdbbf6170b555e606ee7ba9019032c7e
Karthik Sampath (TCS) <k.s10@tcs.com>
2024-01-08 03:34:27 +0530

    Added BaReGa code

--- app/src/main/java/com/example/barega/LevelPassedDialogFragment.kt
+++ app/src/main/java/com/example/barega/LevelPassedDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 
 class LevelPassedDialogFragment : DialogFragment() {
@@ -11,6 +12,17 @@ class LevelPassedDialogFragment : DialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.level_passed, container, false)
+        val view = inflater.inflate(R.layout.level_passed, container, false)
+
+        val okButton = view.findViewById<Button>(R.id.okButton)
+
+        okButton.setOnClickListener {
+            // Handle "OK" button click here
+
+            dismiss() // Close the dialog
+        }
+        // Prevent the dialog from being canceled when touched outside its window
+        dialog?.setCanceledOnTouchOutside(false)
+        return view
     }
 }
